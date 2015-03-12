e61876758030afc828e8cd946321e52a4ceb8ace
Miles Hochstein <miles.hochstein@gmail.com>
2015-03-11 21:03:46 -0700

    simple server and hello world index file

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json())
+ 
+app.get('/', function(req,res){
+  res.sendfile('index.html')
+})
+
+app.listen(3000, function(){
+	console.log('Server listening on port ', 3000)
+})
