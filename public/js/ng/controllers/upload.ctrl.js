331a4cf37ebced8e079a2dc41d904feecd81861d
Miles Hochstein <miles.hochstein@gmail.com>
2015-03-17 10:58:05 -0700

    image upload stuff

---
+++ public/js/ng/controllers/upload.ctrl.js
@@ -0,0 +1,24 @@
+angular.module('pullPix')
+    	.controller('UploadCtrl', [ '$scope', '$upload', function($scope, $upload) {
+  $scope.onFileSelect = function($files) {
+    //$files: an array of files selected, each file has name, size, and type.
+    for (var i = 0; i < $files.length; i++) {
+      var file = $files[i];
+      $scope.upload = $upload.upload({
+        url: '/imageBucket', //upload.php script, node.js route, or servlet url
+        data: {myObj: $scope.myModelObj},
+        file: file,
+      }).progress(function(evt) {
+        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+      }).success(function(data, status, headers, config) {
+        // file is uploaded successfully
+        console.log(data);
+      });
+    }
+  };
+}]);  
+
+
+ 
+ 
+ 
