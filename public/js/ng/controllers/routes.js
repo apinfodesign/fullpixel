265cf8955dd60b2c09adcf13afcf25e8cafd526d
Seth Shober <seth.shober@gmail.com>
2015-03-20 17:25:46 -0700

    starting to mess with static routes

--- public/js/ng/controllers/routes.js
+++ public/js/ng/controllers/routes.js
@@ -5,5 +5,9 @@ angular.module('pullPix')
             .when('/register', {controller: 'RegisterCtrl', templateUrl: '/partials/register.html'})
             .when('/login',    {controller: 'LoginCtrl', templateUrl: '/partials/login.html'})
             .when('/imagemeta',{controller: 'ImgMetaCtrl', templateUrl: '/partials/img-meta.html'})
-            .when('/upload',   {controller: 'UploadCtrl', templateUrl: '/templates/upload.html'}); 
+            .when('/upload',   {controller: 'UploadCtrl', templateUrl: '/templates/upload.html'})
+            .when('/splash',   {controller: ['RegisterCtrl', 'LoginCtrl'], templateUrl: '/partials/splash-page.html'})
+            .when('/photo',    {controller: 'ImgMetaCtrl', templateUrl: '/partials/photo-page.html'})
+            .when('/profile',  {controller: '', templateUrl: '/partials/profile-page.html'}) 
+            .when('/photo-map',{controller: '', templateUrl: '/partials/map-page.html'}); 
     });
