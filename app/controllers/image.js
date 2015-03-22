f9e400695687147151d2c828c3edb4124fac44d3
Miles Hochstein <miles.hochstein@gmail.com>
2015-03-22 13:50:16 -0700

    backed out some image.js changes and began again with cutting GM functions

--- app/controllers/image.js
+++ app/controllers/image.js
@@ -1,7 +1,8 @@
-var router 	= require('express').Router();
-var busboy 	= require('connect-busboy');
-var fs 		= require('fs');
-var gm 		= require('gm');
+var router = require('express').Router();
+var busboy = require('connect-busboy');
+var fs = require('fs');
+var gm = require('gm');
+
 
 router.post('/api/user/upload', function(req, res, next){
 	var fstream;
@@ -11,25 +12,33 @@ router.post('/api/user/upload', function(req, res, next){
 		fstream = fs.createWriteStream('./public/uploads/' + filename);
 		file.pipe(fstream);
 
-		fstream.on('close', function(){
-					console.log('upload stream closed');
-					//res.redirect('back');
-					var cameraModel;
-					gm('./public/uploads/' + filename)
-					.identify(function (err, data) {
-				 	if (!err) {
-				  			console.log("Selected EXIF elements: ");
-		 		  			cameraModel = data["Profile-EXIF"].Model;
-				  			console.log(cameraModel);
-				  			console.log(filename);
-				  			}
-				  	//res.json(cameraModel);
-					});
-				}); //close fstream
+
+		gm('./public/uploads/' + filename)
+		.identify(function (err, data) {
+		  if (!err) {
+		  			console.log("Seleted EXIF elements: ");
+		  			console.log(data.Compression);
+		  			console.log(data.Signature);
+		  			console.log("...............");
+		  			console.log("All EXIF DATA... ");
+		  			console.log(data["Profile-EXIF"].Model);
+		  			} 
 		});
+
+
+		fstream.on('close', function(){
+			console.log('upload stream closed');
+			//res.redirect('back');
+			res.json('woo hoo');
+		});
+	});
+
+
+
 });
+
+ 
+
+
+
 module.exports = router;
-
-
-///  ON fstream.on CLOSE ( do gm find info, THEN res.json takes value of EXIF info.... then elsewhere assign that to ng-upload controller  )
-
