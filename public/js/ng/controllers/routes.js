383ec9a992f80eb4cf402d13811ebb68fbec12b1
Seth Shober <seth.shober@gmail.com>
2015-03-22 17:18:54 -0700

    upload static page stuff

--- public/js/ng/controllers/routes.js
+++ public/js/ng/controllers/routes.js
@@ -9,5 +9,6 @@ angular.module('pullPix')
             .when('/splash',   {controller: ['RegisterCtrl', 'LoginCtrl'], templateUrl: '/partials/splash-page.html'})
             .when('/photo',    {controller: 'ImgMetaCtrl', templateUrl: '/partials/photo-page.html'})
             .when('/profile',  {controller: '', templateUrl: '/partials/profile-page.html'}) 
-            .when('/photo-map',{controller: '', templateUrl: '/partials/map-page.html'}); 
+            .when('/photo-map',{controller: '', templateUrl: '/partials/map-page.html'})
+            .when('/upload-page',{controller: 'UploadCtrl', templateUrl: '/partials/upload-page.html'}); 
     });
