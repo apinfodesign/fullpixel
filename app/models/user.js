fb71f9490f2d3cb37f4609b397ec3b9e5f188db0
Seth Shober <seth.shober@gmail.com>
2015-03-23 18:24:43 -0700

    added in upload static to angular routes/controllers. model updated to reflect.

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,8 @@
 var db =require('./db');
 
 var User = db.model('User',{
-    username: {type: String, required: true},
-    password: {type: String, required: true, select: false}
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true, select: false, min: 8}
 });
 
 module.exports = User;
