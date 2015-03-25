1a3df5a83d33f42dc93f2855f8776006beaa0b77
Miles Hochstein <miles.hochstein@gmail.com>
2015-03-25 10:16:38 -0700

    working decimal lat lon in image.js

--- app/controllers/image.js
+++ app/controllers/image.js
@@ -2,27 +2,7 @@ var router = require('express').Router();
 var busboy = require('connect-busboy');
 var fs = require('fs');
 var gm = require('gm');
-var bufferjs = require('bufferjs');
-
-var degreeToDecimal = function (coord, compass){ 
-	//transorms standard degree min sec EXIF coord to decimal value, latitude or longitude
-	//N and E compass positive, S and W compass negative
-	var direction = 1;  // N or E
-  	var decimalCoord; // return value
-  	var elements = coord.split(",");//should give 3 element array>>> 45/1,31/1,54636/1000
-	var degrees=elements[0].split("/"); //should give 2 element array 45,1 
-    var finalDegrees = degrees[0]/degrees[1];
-    var minutes = elements[1].split("/"); //should give 2 element array 31,1
-    var finalMinutes = minutes[0]/minutes[1];
-    var seconds = elements[2].split("/");  //should give 2 element array 54636/1000
-    var finalSeconds = seconds[0]/seconds[1];
-    if ((compass === "S")|| (compass === "W"))
-    	{direction=-1 }; 
-    decimalCoord = direction * (Math.abs(finalDegrees) + (finalMinutes/60.0) + (finalSeconds / 3600.0) );
-    return decimalCoord;
- };
  
-
 router.post('/api/user/upload', function(req, res, next){
 	var fstream;
 	var cameraModel;
@@ -53,8 +33,6 @@ router.post('/api/user/upload', function(req, res, next){
 			        
 					var latOut = degreeToDecimal(lat, latDirection);
 			        var lonOut = degreeToDecimal(lon, lonDirection);
-
-
 	  		  		}
 	  		  	else{console.log("gm error: " + error);}
 
@@ -65,8 +43,30 @@ router.post('/api/user/upload', function(req, res, next){
   	  			console.log('2 '+ cameraModel);  	//nexus
  		  		
  		  		res.json(data);
- 			});
+ 				})
+ 				.size
+ 				
+
 		});
 	});
 });
+
+var degreeToDecimal = function (coord, compass){ 
+	//transorms standard degree min sec EXIF coord to decimal value, latitude or longitude
+	//N and E compass positive, S and W compass negative
+	var direction = 1;  // N or E
+  	var decimalCoord; // return value
+  	var elements = coord.split(",");//should give 3 element array>>> 45/1,31/1,54636/1000
+	var degrees=elements[0].split("/"); //should give 2 element array 45,1 
+    var finalDegrees = degrees[0]/degrees[1];
+    var minutes = elements[1].split("/"); //should give 2 element array 31,1
+    var finalMinutes = minutes[0]/minutes[1];
+    var seconds = elements[2].split("/");  //should give 2 element array 54636/1000
+    var finalSeconds = seconds[0]/seconds[1];
+    if ((compass === "S")|| (compass === "W"))
+    	{direction=-1 }; 
+    decimalCoord = direction * (Math.abs(finalDegrees) + (finalMinutes/60.0) + (finalSeconds / 3600.0) );
+    return decimalCoord;
+ };
+
 module.exports = router;
--- public/js/ng/controllers/upload.ctrl.js
+++ public/js/ng/controllers/upload.ctrl.js
@@ -4,62 +4,21 @@ angular.module('pullPix')
    $scope.onFileSelect = function(files) {
     //$files: an array of files selected, each file has name, size, and type.
     //   var file = files[0];
-
       $scope.upload = $upload.upload({
         url: '/api/user/upload',  
         method: 'POST',
         data: {myObj: $scope.myModelObj},
         file: files,  //number files uploaded
-
       }).progress(function(evt) {
         console.log(evt.loaded + " is loaded");
         console.log(evt.total + " is total ");
         console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
         console.log(files);
         //console.log(data + " is data ");
-
-      }).success(function(data, status, headers, config) {
+      }).success(function(data, status, headers, config){
         console.log('success fileout');
         // file is uploaded successfully
-        $scope.fileout = files[0].name;
-//        $scope.fileoutSize = files[0].size;
-//        $scope.fileoutLast = files[0].lastModified;
-        $scope.fileoutCamera = data["Profile-EXIF"].Model;
-        $scope.fileoutLongitude = data["Profile-EXIF"]['GPS Latitude'];
-        $scope.fileoutLatitude = data["Profile-EXIF"]['GPS Longitude'];
-
-
-        var latitude = data["Profile-EXIF"]['GPS Latitude'];
-        var latitudeDirection = data["Profile-EXIF"][''];
-        var longitude = data["Profile-EXIF"]['GPS Longitude'];
-        var longitudeDirection = data["Profile-EXIF"][''];
-
-        var finalLongitude = function(latitude, latitudeDirection, longitude, longitudeDirection)
-          {
-          var elements = str.latitude(",");//should give 3 element array 45/1,31/1,54636/1000
-          console.log(elements + " is elements"); 
-            var degrees=str.elements[0]("/"); //should give 2 element array 45,1 
-            var finalDegrees = degrees[0]/degrees[1];
-            var minutes = str.elements[1]("/"); //should give 2 element array 31,1
-            var finalMinutes = minutes[0]/minutes[1];
-            var seconds = str.elements[2]("/");  //should give 2 element array 54636/1000
-            var finalSeconds = seconds[0]/seconds[1];
-
-          //   var demonenator=str.elements
-
-          // var toDecimal = function (number) {
-          //   return number[0].numerator + number[1].numerator /
-          //   (60 * number[1].denominator) + number[2].numerator / (3600 * number[2].denominator);
-          //    };
-
-          // dd = Math.signum(d) * (Math.abs(d) + (m / 60.0) + (s / 3600.0));
-          };
-          finalLongitude(latitude, latitudeDirection, longitude, longitudeDirection);
-
-//$scope.fileoutCamera = JSON.stringify(data);
-//$scope.fileoutCamera =  data.Signature;
-        console.log("data start >>> " + data["Profile-EXIF"] + " <<< data end...from upload.ctrl.js");
-        console.log("successful upload (from upload.ctrl.js)");
+        $scope.fileout = files[0].name;  //to enable image to appear
     });
   };
 });
