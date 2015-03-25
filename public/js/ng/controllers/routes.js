1e31776f819392b35a320f6d31e4f1f3b821b9b6
Michael_Montero <mmontero2012@gmail.com>
2015-03-25 14:58:29 -0700

    upload page integration

--- public/js/ng/controllers/routes.js
+++ public/js/ng/controllers/routes.js
@@ -1,15 +1,12 @@
 angular.module('pullPix')
     .config(function ($routeProvider){
         $routeProvider
-            .when('/',         {controller: 'ListCtrl', templateUrl: '/partials/posts.html'})
+            .when('/',         {controller: 'LoginCtrl', templateUrl: '/partials/splash-page.html'})
             .when('/register', {controller: 'RegisterCtrl', templateUrl: '/partials/register.html'})
             .when('/login',    {controller: 'LoginCtrl', templateUrl: '/partials/login.html'})
-            .when('/imagemeta',{controller: 'ImgMetaCtrl', templateUrl: '/partials/img-meta.html'})
-            .when('/upload',   {controller: 'UploadCtrl', templateUrl: '/templates/upload.html'})
+            .when('/upload',   {controller: 'UploadCtrl', templateUrl: '/partials/upload-page.html'})
             .when('/splash',   {controller: 'RegisterCtrl', templateUrl: '/partials/splash-page.html'})
             .when('/photo',    {controller: 'ImgMetaCtrl', templateUrl: '/partials/photo-page.html'})
             .when('/profile',  {controller: '', templateUrl: '/partials/profile-page.html'}) 
-            .when('/photo-map',{controller: '', templateUrl: '/partials/map-page.html'})
-            .when('/upload-page',{controller: 'UploadCtrl', templateUrl: '/partials/upload-page.html'})
             .when('/photo-map',{controller: '', templateUrl: '/partials/map-page.html'}); 
      });
