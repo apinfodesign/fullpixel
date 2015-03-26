86b1f10f5c17f609d06c410afad1416059b631d6
Seth Shober <seth.shober@gmail.com>
2015-03-25 17:11:10 -0700

    some readability

--- public/js/ng/controllers/upload.ctrl.js
+++ public/js/ng/controllers/upload.ctrl.js
@@ -24,7 +24,30 @@ angular.module('pullPix')
         var latDirection = data["Profile-EXIF"]['GPS Latitude Ref'];        
         var lon = data["Profile-EXIF"]['GPS Longitude'];
         var lonDirection = data["Profile-EXIF"]['GPS Longitude Ref'];
-        $scope.lat =degreeToDecimal(lat, latDirection );
+
+        function degreeToDecimal(coord, compass){ 
+
+          //transorms standard degree min sec EXIF coord to decimal value, latitude or longitude
+          //N and E compass positive, S and W compass negative
+          var direction = 1; // N or E
+          var decimalCoord; // return value
+          var elements = coord.split(",");//should give 3 element array>>> 45/1,31/1,54636/1000
+          var degrees = elements[0].split("/"); //should give 2 element array 45,1 
+          var finalDegrees = degrees[0]/degrees[1];
+          var minutes = elements[1].split("/"); //should give 2 element array 31,1
+          var finalMinutes = minutes[0]/minutes[1];
+          var seconds = elements[2].split("/");  //should give 2 element array 54636/1000
+          var finalSeconds = seconds[0]/seconds[1];
+          
+          if ( compass === "S" || compass === "W" ){ direction = -1 }; 
+          
+          decimalCoord = direction * ( Math.abs(finalDegrees) + (finalMinutes/60.0) + (finalSeconds / 3600.0) );
+          
+          return decimalCoord;
+
+        }
+
+        $scope.lat = degreeToDecimal(lat, latDirection);
         $scope.lon = degreeToDecimal(lon, lonDirection);
 
      });
@@ -32,22 +55,21 @@ angular.module('pullPix')
   $scope.ImgUpdate = function(metadata){
             if(metadata){
                 ImgMetaSvc.create({
-                    userid          : metadata.userid,
-                    path            : metadata.path,
-                    title           : metadata.title,
-                    caption         : metadata.caption,
-                    tags            : metadata.tags,
-                    camera          : metadata.camera,
-                    shutter         : metadata.shutter,
-                    aperture        : metadata.aperture,
-                    iso             : metadata.iso,
-                    date            : metadata.date
+                  userid          : metadata.userid,
+                  path            : metadata.path,
+                  title           : metadata.title,
+                  caption         : metadata.caption,
+                  tags            : metadata.tags,
+                  camera          : metadata.camera,
+                  shutter         : metadata.shutter,
+                  aperture        : metadata.aperture,
+                  iso             : metadata.iso,
+                  date            : metadata.date
                 })
-                    .success(function(imgmeta){
-                        console.table(imgmeta);
-
-                        metadata = null;
-                    });
+                .success(function(imgmeta){
+                  console.table(imgmeta);
+                  metadata = null;
+                });
             }
   };
 });
