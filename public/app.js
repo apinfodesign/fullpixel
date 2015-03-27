cd5f580dd7e551eab6774ff54a488dcd27d6e260
MattCordeiro <matthew3cord@gmail.com>
2015-03-27 14:14:05 -0700

    App.js.

--- public/app.js
+++ public/app.js
@@ -142,7 +142,24 @@ angular.module('pullPix')
 
   };
 }]);
-
+angular.module('pullPix')
+    .controller('MemberListCtrl', ["MemberSvc", function(MemberSvc){
+        var vm =this;
+        vm.MemberListAdd = function(){
+            if(vm.username){
+                MemberSvc.create({
+                    body: vm.username
+                })
+                    .success(function(member){
+                        vm.members.unshift(member);
+                    })
+            }
+        };
+        MemberSvc.fetch()
+            .success(function(members){
+                vm.members = members
+            });
+    }]);
 
 
 angular.module('pullPix')
