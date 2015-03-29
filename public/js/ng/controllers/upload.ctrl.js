c4c7b3ac928a5a8fb2a686dbfb9f194eb89d3e87
Miles Hochstein <miles.hochstein@gmail.com>
2015-03-29 16:05:28 -0700

    fixed broken google map with static map api and temp delete of currentUser

--- public/js/ng/controllers/upload.ctrl.js
+++ public/js/ng/controllers/upload.ctrl.js
@@ -1,14 +1,11 @@
-
 angular
   .module('pullPix')
   .controller('UploadCtrl', Upload);
-
 
   Upload.$inject = ['$upload', 'ImgMetaSvc', '$location'];
 
   function Upload($upload, ImgMetaSvc, $location) {
  
-
     var vm = this;
     vm.fileout = null;
     vm.currentuser = null;
@@ -17,7 +14,6 @@ angular
     vm.upload = null;
     vm.onFileSelect = onFileSelect;
     vm.imgUpdate = imgUpdate;
-
 
    function onFileSelect(files) {
  
@@ -29,12 +25,14 @@ angular
 
       }).progress(function(evt) {
 
-
       }).success(function(data, status, headers, config) {
+        console.log('success fileout 1');
+
+
         vm.fileout = "/uploads/" + files[0].name;
-        vm.currentuser = CurrentUser.userid;
+        //TEMP DELETE NOT WORKING vm.currentuser = CurrentUser.userid;
        
-        console.log('success fileout');
+        console.log('success fileout 2');
  
         // convert deg to dec here
         var lat = data["Profile-EXIF"]['GPS Latitude'];
@@ -45,24 +43,24 @@ angular
         var degreeToDecimal = function (coord, compass){ 
           if (coord != null && compass != null)  //handles missing EXIF data error
           {
-            //transorms standard degree min sec EXIF coord to decimal value, latitude or longitude
+            //transforms standard degree min sec EXIF coord to decimal value, latitude or longitude
             //N and E compass positive, S and W compass negative
             var direction = 1;  // N or E
-              var decimalCoord; // return value
-              var elements = coord.split(",");//should give 3 element array>>> 45/1,31/1,54636/1000
+            var decimalCoord; // return value
+            var elements = coord.split(",");//should give 3 element array>>> 45/1,31/1,54636/1000
             var degrees=elements[0].split("/"); //should give 2 element array 45,1 
-              var finalDegrees = degrees[0]/degrees[1];
-              var minutes = elements[1].split("/"); //should give 2 element array 31,1
-              var finalMinutes = minutes[0]/minutes[1];
-              var seconds = elements[2].split("/");  //should give 2 element array 54636/1000
-              var finalSeconds = seconds[0]/seconds[1];
-              if ((compass === "S")|| (compass === "W"))
-                {direction=-1 }; 
-              decimalCoord = direction * (Math.abs(finalDegrees) + (finalMinutes/60.0) + (finalSeconds / 3600.0) );
+            var finalDegrees = degrees[0]/degrees[1];
+            var minutes = elements[1].split("/"); //should give 2 element array 31,1
+            var finalMinutes = minutes[0]/minutes[1];
+            var seconds = elements[2].split("/");  //should give 2 element array 54636/1000
+            var finalSeconds = seconds[0]/seconds[1];
+            if ((compass === "S")|| (compass === "W"))
+              {direction=-1 }; 
+            decimalCoord = direction * (Math.abs(finalDegrees) + (finalMinutes/60.0) + (finalSeconds / 3600.0) );
           }
           return decimalCoord;
        };
-
+        console.log("generate lat lon");
         vm.lat = degreeToDecimal(lat, latDirection);
         vm.lon = degreeToDecimal(lon, lonDirection);
 
