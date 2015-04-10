cee4139ad0a1fd56fb97c651da10a12f39c908b5
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-10 16:45:34 -0700

    set up about page with new user info - not complete

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,12 @@ var db =require('./db');
 var User = db.model('User',{
     username:   {type: String, required: true, unique: true},
     password:   {type: String, required: true, select: false, min: 8},
-    userphoto:  {type: String}
+    userphoto:  {type: String},
+    userportrait: {type: String},
+    userblogtitle: {type: String, unique: true},
+    useraboutstory: {type: String, max: 2000},
+    usertags: {type: String}
+
 });
 
 module.exports = User;
