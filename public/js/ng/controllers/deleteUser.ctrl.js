33e75f5220df3f79ab40658b89f52b61cc9b355c
Michael_Montero <mmontero2012@gmail.com>
2015-04-21 12:22:08 -0700

    minor changes

--- public/js/ng/controllers/deleteUser.ctrl.js
+++ public/js/ng/controllers/deleteUser.ctrl.js
@@ -2,15 +2,15 @@ angular.module('pullPix')
     .controller('deleteUserCtrl',  
         function($http, $scope){
  		
-        $scope.UserDelete = function(username){
-            $http.delete('/users')
+        $scope.UserDelete = function(currentUser){
+            return $http.delete('/users', currentUser)
             .success(function(data) {
                 console.log('success');
-                $scope.username = data;
+                //$scope.currentUser = data;
             })
             .error(function(err) {
                 console.log('Error: ' + data);
-                $scope.username = err;
+                //$scope.currentUser = err;
             });
          };
     });
