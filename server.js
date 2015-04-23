8b656be1c1c8ba12aa4b604ea472314d4819ec04
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-23 15:17:03 -0700

    heroku server.js configuration

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(require('./app/controllers/image'));
 app.use(require('./app/controllers/img-meta'));
 app.use(express.static('./public'));
 
-app.listen(3000, function () {
-   console.log('Server is listening on port ', 3000) ;
-});
+// app.listen(3000, function () {
+//    console.log('Server is listening on port ', 3000) ;
+// });
 //used for heroku
 
-// app.listen(process.env.PORT);
+app.listen(process.env.PORT);
 
 
 
