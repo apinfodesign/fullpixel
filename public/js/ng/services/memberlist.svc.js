3aea9256704662d7138d4e00f4ab32f0119d039e
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-25 16:30:28 -0700

    change member list svc to https?

--- public/js/ng/services/memberlist.svc.js
+++ public/js/ng/services/memberlist.svc.js
@@ -1,6 +1,6 @@
 angular.module('pullPix')
     .service('MemberListSvc', function($http){
         this.fetch = function(){
-            return $http.get('http://localhost:3000/member');
+            return $http.get('https://localhost:3000/member');
         }
     });
