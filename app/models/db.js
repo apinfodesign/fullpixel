eeade0b87fcd37bba732fb596a7863905a54451c
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-25 15:07:01 -0700

    port error fixed to 5000

--- app/models/db.js
+++ app/models/db.js
@@ -17,7 +17,7 @@ var mongolabConnectString = "MONGOLAB_URI"
 // The http server will listen to an appropriate port, or default to
 // port 5000.
 var uristring = process.env.MONGOLAB_URI;	
-var theport = process.env.PORT || 5	000;
+var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
