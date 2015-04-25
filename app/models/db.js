a863c1909bbbab237de87483f6b78d83d0c6dcaa
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-25 15:19:12 -0700

    uristring adjust

--- app/models/db.js
+++ app/models/db.js
@@ -12,20 +12,22 @@ var db = mongoose.connection;
 //mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
 
 //FOR HEROKU ENVIRONMENT ONLY
-var mongolabConnectString = "MONGOLAB_URI"
+//var mongolabConnectString = MONGOLAB_URI;
+
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
 var uristring = process.env.MONGOLAB_URI;	
 var theport = process.env.PORT || 5000;
 
+console.log(uristring + " is uristring!!!	");
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
   if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  console.log ('ERROR connecting to::: ' + uristring + '. ' + err);
   } else {
-  console.log ('Succeeded connected to: ' + uristring);
+  console.log ('Succeeded connected to::: ' + uristring);
   }
 });
  
