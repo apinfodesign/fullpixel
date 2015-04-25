027a9b4b6f218d2666d50296ce41a3485bfb0eb9
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-24 18:06:51 -0700

    heroku mongoose mongolab model

--- app/models/db.js
+++ app/models/db.js
@@ -1,22 +1,29 @@
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-
-//EXAMPLE OF MONGOLAB CONNECT STRING
-//mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
-
+ 
 //FOR LOCAL DEPLOYMENT
 //deleted for deployment
  
-
 //FOR HEROKU ENVIRONMENT ONLY
-var mongolabConnectString = "MONGOLAB_URI";	
+var uristring = process.env.MONGOLAB_URI;	
+var theport = process.env.PORT || 5000;
 
-mongoose.connect( mongolabConnectString , function(){
-	db.on('error', console.error.bind(console, 'connection error:')); //not logging error
-
-    db.once('open', function(){
-        console.log("Successfully connected to MongoDB for FullPixel at Monglolabs.com");
-    });
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
 });
+
+// mongoose.connect( uristring , function(){
+// 	db.on('error', console.error.bind(console, 'connection error:')); //not logging error
+
+//     db.once('open', function(){
+//         console.log("Successfully connected to MongoDB for FullPixel at Monglolabs.com");
+//     });
+// });
 module.exports = mongoose;
 
