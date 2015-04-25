eb21da77029c0398edf01214462afafcb7dbd3c0
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-25 14:57:55 -0700

    check gitignore valid - yes

--- app/models/db.js
+++ app/models/db.js
@@ -1,28 +1,26 @@
-var http = require ('http');  //necssary?
+var http = require ('http');  //necessary?
 
 var mongoose = require('mongoose');
 var db = mongoose.connection;
  
-
+//FOR LOCAL DEPLOYMENT
 //keep mongolabs in .gitignore external file never uploaded to github
-var connectstring = require('./mongolabinfo.js');
- console.log("Secret connectstring is: " + connectstring.name);
+//this is for local testing only
+
+//var connectstring = require('./mongolabinfo.js');
+//console.log("Secret connectstring is: " + connectstring.name);
+//var mongolabConnectString=connectstring.name;	
 
 //EXAMPLE OF MONGOLAB CONNECT STRING
 //mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
 
-//FOR LOCAL DEPLOYMENT
-//Deleted for deployment
-var mongolabConnectString=connectstring.name;	
-
 //FOR HEROKU ENVIRONMENT ONLY
-//var mongolabConnectString = "MONGOLAB_URI"
- 
+var mongolabConnectString = "MONGOLAB_URI"
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
 var uristring = process.env.MONGOLAB_URI;	
-var theport = process.env.PORT || 5000;
+var theport = process.env.PORT || 5	000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
