7b11a3b299a52856631e65ff98fe70c8d2a44ba3
Miles Hochstein <miles.hochstein@gmail.com>
2015-04-27 16:31:42 -0700

    conditional logic change for heroku versus local w try catch

--- app/models/db.js
+++ app/models/db.js
@@ -1,36 +1,34 @@
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-//var connectstring = require('./mongolabinfo.js');
+var uristring;
 
-//FOR LOCAL DEPLOYMENT
-//keep mongolabs in .gitignore external file never uploaded to github
-//this is for local testing only
+//require('./mongolabinfo.js');  //if local env
 
-//EXAMPLE OF MONGOLAB CONNECT STRING
-//mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
-			
-//FOR HEROKU ENVIRONMENT ONLY
-//var mongolabConnectString = MONGOLAB_URI;
 
-var uristring = process.env.MONGOLAB_URI;
+try{
+	//require('./mongolabinfo.js');
+	uristring = require('./mongolabinfo.js').name;
+}
+catch(err){
+	console.log("no connection file so go on to Heroku config var")
+	uristring = process.env.MONGOLAB_URI;   //if Heroku env
+}
 
-if (!uristring){
-	uristring = connectstring.name;
-	console.log('not reading Heroku config var so local is happening');
-};
+
+// if (!uristring){   //if operating in local env
+// 	uristring = connectstring.name;
+// 	console.log('not reading Heroku config var so local is happening');
+// };
+//otherwise, we are in Heroku env
 
 console.log("uristring is "+ uristring);
- // Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
 
 mongoose.connect( uristring , function(){
-	db.on('error', console.error.bind(console, 'connection error:')); //not logging error
-
+	db.on('error', console.error.bind(console, 'connection error:')); 
+	//not logging error
     db.once('open', function(){
         console.log("Successfully connected to MongoDB for FullPixel at Monglolabs.com");
     });
 });
  
-
-
 module.exports = mongoose;
